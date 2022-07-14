1b3a9e683d601e148e5787ce8bcbe02c234ea432
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-07-14 20:07:11 +0300

    add capitalize & test

---
+++ src/capitalize.js
@@ -0,0 +1,9 @@
+export default (text) => {
+  if (text === '') {
+    return '';
+  }
+
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
