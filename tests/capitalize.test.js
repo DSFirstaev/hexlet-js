8bac941a9ce7b9b54b0f0ab465c850c63baa5f0e
Dmitry_Firstaev <dmitry.firstaev@gmail.com>
2022-07-15 19:48:21 +0300

    if -->assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,9 @@
+import { strict as assert } from 'assert';
+
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize(''), '');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize('hello'), 'Hello');
 
 console.log('Все тесты пройдены!');
